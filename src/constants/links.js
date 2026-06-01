df6f06a34dfe9f6da623479ca9623457bee3db47
Agustin Delgado <augusto.delgado00@hotmail.com>
2026-06-01 02:17:29 -0300

    chore(portfolio): enable Halo Brief public URL

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,5 +1,5 @@
 /** Reemplazar por la URL de deploy cuando Halo Brief esté en producción */
-export const HALO_BRIEF_URL = '#'
+export const HALO_BRIEF_URL = 'https://paradise-paradise-halo.vercel.app/'
 
 export function isHaloBriefLive(url = HALO_BRIEF_URL) {
   return Boolean(url && url !== '#')
